d601f98651dd9fc95de435215bd10080cdffee0a
Ankit <83949374+devankit57@users.noreply.github.com>
2023-07-13 22:58:15 +0530

    Add files via upload

---
+++ build/precache-manifest.082d5b3d71ddbe7f597b56efe967c722.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4a19da63ccfc199dcab",
+    "url": "/static/css/main.d666adc1.chunk.css"
+  },
+  {
+    "revision": "c4a19da63ccfc199dcab",
+    "url": "/static/js/main.c4a19da6.chunk.js"
+  },
+  {
+    "revision": "f4a2a6c05474266edc2e",
+    "url": "/static/js/1.f4a2a6c0.chunk.js"
+  },
+  {
+    "revision": "452be30078ec58129b14",
+    "url": "/static/css/2.ef0448bb.chunk.css"
+  },
+  {
+    "revision": "452be30078ec58129b14",
+    "url": "/static/js/2.452be300.chunk.js"
+  },
+  {
+    "revision": "6f2903f8d453d711a5fa",
+    "url": "/static/js/runtime~main.6f2903f8.js"
+  },
+  {
+    "revision": "69f860c3ccb57a90e668e45deae145f8",
+    "url": "/static/media/head.69f860c3.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "31a0928a2f11ad44cd0200eed9b76cd1",
+    "url": "/static/media/City of Tomorrow.31a0928a.jpeg"
+  },
+  {
+    "revision": "a40dcf24a7a63e5eebf7b60ab8aac073",
+    "url": "/static/media/Underwater Paradis.a40dcf24.jpeg"
+  },
+  {
+    "revision": "f4f35916140a8933bb6f538aa387117f",
+    "url": "/static/media/Magical Creatures.f4f35916.jpeg"
+  },
+  {
+    "revision": "5efb00375773ccfd11da13487c3fce62",
+    "url": "/static/media/Time Travel Adventure.5efb0037.jpeg"
+  },
+  {
+    "revision": "74edc744a559da1380debf14a9ba5fc3",
+    "url": "/static/media/Celebration of Life.74edc744.jpeg"
+  },
+  {
+    "revision": "a8700674982f2470a454e7c019238b79",
+    "url": "/index.html"
+  }
+];
